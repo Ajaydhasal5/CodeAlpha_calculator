da9b5614f17af12451596796ae86f42e08f0dfaf
Ajay Dhasal <dhasalajay590@gmail.com>
2026-02-04 09:36:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+document.addEventListener("keydown", function (event) {
+
+  if ((event.key >= "0" && event.key <= "9") || "+-*/.".includes(event.key)) {
+    appendValue(event.key);
+  }
+
+  if (event.key === "Enter") {
+    calculate();
+  }
+
+  if (event.key === "Backspace") {
+    deleteLast();
+  }
+
+  if (event.key === "Escape") {
+    clearDisplay();
+  }
+
+});
